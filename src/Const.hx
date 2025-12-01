d7258c335d02353e927ebaa61dd862d1106a0dba
tgbfb <144910828+tgbfb@users.noreply.github.com>
2025-12-01 13:47:48 +0300

    AI: create src/Const.hx — создать игру

---
+++ src/Const.hx
@@ -0,0 +1,7 @@
+package;
+
+class Const {
+    public static inline var TILE_SIZE : Int = 32;
+    public static inline var MAP_WIDTH : Int = 25;
+    public static inline var MAP_HEIGHT : Int = 18;
+}
